38220a2a12b65cd156124c263d233a110e6da682
ChuluSimmama <phatusimama@gmail.com>
2024-03-15 17:00:03 +0200

    pets

---
+++ script.js
@@ -0,0 +1,18 @@
+function validateSyntax() {
+    
+    var inputValue = document.getElementById("petInput").value.trim();
+
+   
+    var regex = /^pet_\d{4}[a-zA-Z]+$/;
+
+
+    if (regex.test(inputValue)) {
+        document.getElementById("result").textContent = "Valid Syntax ✅";
+        document.getElementById("result").className = "valid";
+    } else {
+        document.getElementById("result").textContent = "Invalid Syntax ❌";
+        document.getElementById("result").className = "invalid";
+    }
+}
+
+
